4d15afa4497185551adc943fb24d43db3bdb1a1d
erwin-hornell <erwin.hornell.6814@student.uu.se>
2025-10-20 15:06:50 +0200

    added - xor encryption

--- cryptography.py
+++ cryptography.py
@@ -5,13 +5,15 @@ def generate_key(salt: str, password: str) -> str:
 
     return "salted password"
 def cycled_key(s:str, key:str) -> str:
+    #Returns a key the same length as the string s
     return key*(len(s)//len(key)) + key[0:len(s)%len(key)]
 
 def xor_encrypt(s: str, key: str) -> bytes:
-    bts = [(a^b) for a, b in zip(s, key)]
+    key = cycled_key(s, key)
+    bts = "".join(chr(ord(a)^ord(b)) for a, b in zip(s, key)).encode("utf-8")
     return bts
 
-def xor_decrypt(s: str, key: str) -> bytes:
+def xor_decrypt(s: bytes, key: str) -> str:
     pass
 
 def vigenère_encrypt(s: str, key: str) -> str:
@@ -24,4 +26,5 @@ if __name__ == "__main__":
     key = "somekey"
     s = "short"
     print(s)
-    print(cycled_key(s, key))
+    print(xor_encrypt(s, key))
+    print(xor_encrypt(s, key))
