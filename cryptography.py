5786418e3312fe5e2c3bdec08671372e2c412ae1
erwin-hornell <erwin.hornell.6814@student.uu.se>
2025-10-20 14:48:02 +0200

    Added a way of cycling key

--- cryptography.py
+++ cryptography.py
@@ -2,10 +2,14 @@ import os
 import hashlib
 
 def generate_key(salt: str, password: str) -> str:
+
     return "salted password"
+def cycled_key(s:str, key:str) -> str:
+    return key*(len(s)//len(key)) + key[0:len(s)%len(key)]
 
 def xor_encrypt(s: str, key: str) -> bytes:
-    pass
+    bts = [(a^b) for a, b in zip(s, key)]
+    return bts
 
 def xor_decrypt(s: str, key: str) -> bytes:
     pass
@@ -15,3 +19,9 @@ def vigenère_encrypt(s: str, key: str) -> str:
 
 def vigenère_decrypt(s: str, key: str) -> str:
     pass
+
+if __name__ == "__main__":
+    key = "somekey"
+    s = "short"
+    print(s)
+    print(cycled_key(s, key))
